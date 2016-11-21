bb2bf3588af95d82aab494f56c3e8c5a7ae268f2
kosta <tn15hdg@gmail.com>
2016-11-21 09:09:47 +0900

    TaskStore Interface 수정

--- TeamphonyFinal/src/teamphony/store/facade/TaskStore.java
+++ TeamphonyFinal/src/teamphony/store/facade/TaskStore.java
@@ -1,5 +1,21 @@
 package teamphony.store.facade;
 
+import java.util.List;
+
+import teamphony.domain.Task;
+
 public interface TaskStore {
 
+	void insertTask(Task task);
+
+	void updateTask(Task task);
+
+	void deleteTask(int taskId);
+
+	List<Task> selectAllTask();
+
+	Task selectTaskByTaskId(int taskId);
+
+	List<Task> selectTaskByMemberId(String memberId);
+	
 }
--- TeamphonyFinal/src/teamphony/store/logic/TaskStoreLogic.java
+++ TeamphonyFinal/src/teamphony/store/logic/TaskStoreLogic.java
@@ -1,7 +1,46 @@
 package teamphony.store.logic;
 
+import java.util.List;
+
+import teamphony.domain.Task;
 import teamphony.store.facade.TaskStore;
 
 public class TaskStoreLogic implements TaskStore {
+
+	@Override
+	public void insertTask(Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTask(Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteTask(int taskId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Task> selectAllTask() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Task selectTaskByTaskId(int taskId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> selectTaskByMemberId(String memberId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
