18f981f99373a98e3ac0871666a2c8d38300ad8d
민정 <jin21239@gmail.com>
2016-11-25 19:25:22 +0900

    js파일 추가 및 scheduleController 구현

--- TeamphonyFinal/src/teamphony/controller/ScheduleController.java
+++ TeamphonyFinal/src/teamphony/controller/ScheduleController.java
@@ -1,6 +1,7 @@
 package teamphony.controller;
 
 import java.sql.Date;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
@@ -20,41 +21,51 @@ public class ScheduleController {
 	@Autowired
 	private ScheduleService scheduleService;
 	
-	@RequestMapping(value="create.do", method=RequestMethod.POST)
+	@RequestMapping(value="create.do")
 	public String createSchedule(Schedule schedule, HttpSession session){
-		
 		schedule.setTeamCode((String) session.getAttribute("teamCode"));
 		scheduleService.registerSchedule(schedule);
 		return "redirect:/calendar2";
 	}
 	
-	@RequestMapping("revise.do")
+	@RequestMapping(value="revise.do", method=RequestMethod.GET)
+	public String modifySchedule(int scheduleId, Model model){
+		Schedule schedule = scheduleService.findScheduleByScheduleId(scheduleId);
+		model.addAttribute("schedule", schedule);
+		return null;
+	}
+	
+	@RequestMapping(value="revise.do", method=RequestMethod.POST)
 	public String modifySchedule(Schedule schedule){
-		
+		scheduleService.modifySchedule(schedule);
 		return null;
 	}
 	
 	@RequestMapping("erase.do")
 	public String eraseSchedule(int scheduleId){
-		
+		scheduleService.removeSchedule(scheduleId);
 		return null;
 	}
 	
 	@RequestMapping("detail.do")
 	public String searchScheduleByScheduleId(int scheduleId, Model model){
-		
+		Schedule schedule = scheduleService.findScheduleByScheduleId(scheduleId);
+		model.addAttribute("schedule", schedule);
 		return null;
 	}
 	
 	@RequestMapping("calendar.do")
 	public String searchSchedulesByTeamCode(HttpSession session, Model model){
-		
+		String teamCode = (String) session.getAttribute("teamCode");
+		List<Schedule> teamSchedules = scheduleService.findSchedulesByTeamCode(teamCode);
+		model.addAttribute("teamSchedules", teamSchedules);
 		return null;
 	}
 	
 	@RequestMapping("list.do")
 	public String searchSchedulesByDate(Date startDate, Model model){
-		
+		List<Schedule> dateSchedules = scheduleService.findSchedulesByDate(startDate);
+		model.addAttribute("dateSchedules", dateSchedules);
 		return null;
 	}
 	
